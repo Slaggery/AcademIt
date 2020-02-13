0886f88306597d98afe31c34e07814232f858bcb
Кузьмин Дмитрий <slaggery@gmail.com>
2020-02-13 21:20:30 +0700

    Добавил в Range функции пересечения, объединения и разности двух интервалов

--- Range/src/ru/academits/kuzmin/range/Range.java
+++ Range/src/ru/academits/kuzmin/range/Range.java
@@ -32,4 +32,75 @@ public class Range {
     public boolean isInside(double number) {
         return from <= number && number <= to;
     }
+
+    public Range getIntervalsIntersection(Range diapason1, Range diapason2) {
+        double resultFrom;
+        double resultTo;
+
+        if (diapason1.isInside(diapason2.getFrom())) {
+            resultFrom = diapason2.getFrom();
+        } else if (diapason2.isInside(diapason1.getFrom())) {
+            resultFrom = diapason1.getFrom();
+        } else {
+            return null;
+        }
+
+        if (diapason1.isInside(diapason2.getTo())) {
+            resultTo = diapason2.getTo();
+        } else {
+            resultTo = diapason1.getTo();
+        }
+
+        return new Range(resultFrom, resultTo);
+    }
+
+    public Range[] getIntervalsMerge(Range diapason1, Range diapason2) {
+        double resultFrom;
+        double resultTo;
+
+        if (diapason1.isInside(diapason2.getFrom())) {
+            resultFrom = diapason1.getFrom();
+        } else if (diapason2.isInside(diapason1.getFrom())) {
+            resultFrom = diapason2.getFrom();
+        } else {
+            Range[] ranges = new Range[2];
+
+            ranges[0] = new Range(diapason1.getFrom(), diapason1.getTo());
+            ranges[1] = new Range(diapason2.getFrom(), diapason2.getTo());
+            return ranges;
+        }
+
+        if (diapason1.isInside(diapason2.getTo())) {
+            resultTo = diapason1.getTo();
+        } else {
+            resultTo = diapason2.getTo();
+        }
+
+        Range[] result = new Range[1];
+
+        result[0] = new Range(resultFrom, resultTo);
+
+        return result;
+    }
+
+    public Range[] getIntervalsDifference(Range diapason1, Range diapason2) {
+        if (diapason2.getFrom() >= diapason1.getTo() || diapason2.getTo() >= diapason1.getTo()) {
+            Range[] ranges = new Range[1];
+
+            if (diapason2.getFrom() >= diapason1.getTo()) {
+                ranges[0] = new Range(diapason1.getFrom(), diapason1.getTo());
+            } else {
+                ranges[0] = new Range(diapason1.getFrom(), diapason2.getFrom());
+            }
+
+            return ranges;
+        } else {
+            Range[] ranges = new Range[2];
+
+            ranges[0] = new Range(diapason1.getFrom(), diapason2.getFrom());
+            ranges[1] = new Range(diapason2.getTo(), diapason1.getTo());
+
+            return ranges;
+        }
+    }
 }
--- Range/src/ru/academits/kuzmin/range_main/Main.java
+++ Range/src/ru/academits/kuzmin/range_main/Main.java
@@ -2,27 +2,39 @@ package ru.academits.kuzmin.range_main;
 
 import ru.academits.kuzmin.range.Range;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Range diapason = new Range(1, 10);
 
-        System.out.println("Введите чило начала диапазона:");
-        double from = scanner.nextDouble();
+        double inspectionNumber = 7;
 
-        System.out.println("Введите число окончания диапазона:");
-        double to = scanner.nextDouble();
-
-        System.out.println("Введите число для проверки принадлежности диапазону:");
-        double number = scanner.nextDouble();
-
-        Range range = new Range(from, to);
-
-        if (range.isInside(number)) {
-            System.out.printf("Число %.2f входит в диапазон от %.2f до %.2f", number, from, to);
+        if (diapason.isInside(inspectionNumber)) {
+            System.out.printf("Число %.2f входит в диапазон от %.2f до %.2f%n", inspectionNumber, diapason.getFrom(), diapason.getTo());
         } else {
-            System.out.printf("Число %.2f не входит в диапазон от %.2f до %.2f", number, from, to);
+            System.out.printf("Число %.2f не входит в диапазон от %.2f до %.2f%n", inspectionNumber, diapason.getFrom(), diapason.getTo());
+        }
+
+        Range diapason1 = new Range(1, 12);
+        Range diapason2 = new Range(6, 9);
+
+        Range intervalsIntersection = new Range(0, 0).getIntervalsIntersection(diapason1, diapason2);
+
+        if (intervalsIntersection == null) {
+            System.out.println("Диапазона пересечения нет");
+        } else {
+            System.out.printf("Новый диапазон пересечения от %.2f до %.2f%n", intervalsIntersection.getFrom(), intervalsIntersection.getTo());
+        }
+
+        Range[] intervalsMerge = new Range(0, 0).getIntervalsMerge(diapason1, diapason2);
+
+        for (Range range : intervalsMerge) {
+            System.out.printf("Новый диапазон объединения от %.2f до %.2f%n", range.getFrom(), range.getTo());
+        }
+
+        Range[] intervalsDifference = new Range(0, 0).getIntervalsDifference(diapason1, diapason2);
+
+        for (Range range : intervalsDifference) {
+            System.out.printf("Новый диапазон разности от %.2f до %.2f%n", range.getFrom(), range.getTo());
         }
     }
 }
